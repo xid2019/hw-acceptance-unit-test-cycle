d16bee53dc9059995057e72a7c494c1054c9155e
EC2 Default User <ec2-user@ip-172-31-92-55.ec2.internal>
2021-03-09 02:04:10 +0000

    complete

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -23,3 +23,17 @@ Then /I should see all the movies/ do
     step %{I should see "#{movie.title}"}
   end
 end
+
+# Then /the director of "(.*)" should be "(.*)"/ do |movie, director|
+#   expect(Movie.find_by_title(movie).director).to eq(director)
+# end
+
+Then /^the director of "([^"]*)" should be "([^""]*)"$/ do |movie_title, director_name|
+  if page.respond_to? :should
+    page.should have_content(movie_title)
+    page.should have_content(director_name)
+  else
+    assert page.has_content?(movie_title)
+    assert page.has_content?(director_name)
+  end
+end
